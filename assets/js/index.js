4b5cea1e1ebbc7d09a6e8c7db1412348611b65c7
Viktoriia <pecourses>
2023-09-17 18:06:48 +0300

    Add attribetes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,50 @@
-// Обробка подій
+// Атрибути
 
-const btn = document.getElementById('clickBtn');
+// Через властивості
+const p1 = document.querySelector('#p1');
 
-console.log('btn :>> ', btn);
-console.dir(btn);
+// атрибути елементів - це властивості відповідних об'єктів
+console.log(p1.title);
+// вивести id у p1
+console.log(p1.id);
 
-const clickHandler = function (event) {
-  console.log('Btn was clicked');
+p1.title = 'New title';
+// булевські атрибути
+// p1.hidden = true;
+
+p1.style.color = 'red';
+p1.style.backgroundColor = 'orange';
+
+// Через методи
+const p2 = document.querySelector('#p2');
+p2.setAttribute('title', 'Title of p2'); // <p id="p2" title="Title of p2"
+p2.setAttribute('style', 'font-size: 10px; color: yellow;'); // style="font-size: 10px; color: yellow;"
+// p2.setAttribute('hidden', 'hidden'); // <p hidden ...
+
+console.log(p2.getAttribute('style'));
+
+// Задати для body колір тла на вибір
+
+// document.querySelector('body').style.backgroundColor = 'green';
+
+// const body = document.querySelector('body');
+// body.style.backgroundColor = 'green';
+
+document.body.setAttribute('style', 'background-color: green;');
+
+// зміна контенту в елементі
+p2.textContent = 'New content';
+
+// Задати атрибути зображення по об'єкту
+const imgAttr = {
+  src: 'https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_640.jpg',
+  alt: 'cat',
 };
 
-btn.onclick = clickHandler;
+const imgEl = document.querySelector('img');
 
-btn.onclick = null;
-//------------------------------------------------
-
-const loginBtn = document.getElementById('loginBtn');
-
-function loginBtnHandler(e) {
-  alert('Log in in process');
-}
-
-loginBtn.addEventListener('click', loginBtnHandler);
-
-loginBtn.removeEventListener('click', loginBtnHandler);
-
-//---------------------------------------------------
-// Виводити "Click me!", коли наводимо курсором миші на loginBtn
-
-function loginMouseMoveHandler(e) {
-  console.log('Click me!');
-}
-
-loginBtn.addEventListener('mousemove', loginMouseMoveHandler);
-
-// Додати кнопку і при натисканні на неї виводити "Hello world!"
-const greetingsBtn = document.getElementById('greetingsBtn');
-function greetingsBtnHandler(e) {
-  alert('Hello world');
-}
-greetingsBtn.addEventListener('click', greetingsBtnHandler);
+imgEl.src = imgAttr.src;
+imgEl.alt = imgAttr.alt;
+// задати розміри зображенню
+// imgEl.setAttribute('width', '400');
+imgEl.setAttribute('style', 'width: 400px;');
