2740ccdddf108278f68f4c1834c8bd5bfa998d1a
Viktoriia <pecourses>
2023-09-23 16:09:45 +0300

    Ass simple counter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,25 @@
-'use strict';
+// View - Spagetti
 
-const slides = [
-  {
-    src: 'https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape1',
-  },
-  {
-    src: 'https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape2',
-  },
-  {
-    src: 'https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape3',
-  },
-  {
-    src: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    alt: 'landscape4',
-  },
-];
+// View - *** - Model
 
-const slideImg = document.querySelector('img');
-const [prevBtn, nextBtn] = document.querySelectorAll('.navBtn');
+const [decBtn, incBtn] = document.querySelectorAll('.counterBtn');
+const counterEl = document.querySelector('#counter');
 
-try {
-  // Виносимо відповідальність за обчислення для слайдера на клас
-  const slider = new Slider(slides);
+// Model
+let counter = 0;
 
-  updateSlideImage(slider.currentSlide);
+counterEl.textContent = counter;
 
-  function nextSlideHandler() {
-    slider.incSlideIndex();
-    updateSlideImage(slider.currentSlide);
-  }
-  nextBtn.addEventListener('click', nextSlideHandler);
-
-  function prevSlideHandler() {
-    slider.decSlideIndex();
-    updateSlideImage(slider.currentSlide);
-  }
-  prevBtn.addEventListener('click', prevSlideHandler);
-
-  function updateSlideImage(currentSlide) {
-    // currentSlide - об'єкт {src, alt}
-    slideImg.src = currentSlide.src;
-    slideImg.alt = currentSlide.alt;
-
-    slideImg.onerror = () => {
-      slideImg.src =
-        'https://kaverisias.com/kv-app/uploads/2018/01/catalog-default-img.gif';
-    };
-  }
-} catch (e) {
-  slideImg.src =
-    'https://kaverisias.com/kv-app/uploads/2018/01/catalog-default-img.gif';
+function decCount(e) {
+  counter--;
+  counterEl.textContent = counter;
 }
+
+decBtn.addEventListener('click', decCount);
+
+function incCount(e) {
+  counter++;
+  counterEl.textContent = counter;
+}
+
+incBtn.addEventListener('click', incCount);
