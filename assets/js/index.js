5758ae2b8cf0f5d009578fc04d13a614241fd964
Viktoriia <pecourses>
2023-09-23 17:58:58 +0300

    Add classList

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,12 @@
-// Керування розповсюдженням подій
+const div = document.querySelector('.div');
 
-const body = document.body;
-const section = document.querySelector('section');
-const button = document.querySelector('button');
-
-function clickHandler(e) {
-  console.log('this :>> ', this); // = currentTarget
-  // console.log('e.target :>> ', e.target); // ціль події, найбільш занурений елемент
-  // console.log('e.currentTarget :>> ', e.currentTarget);
-  console.log('--------------------------------------');
-}
-
-// Bubbling and capturing / порядок виклику обробників
-
-// на занурення (capturing)
-body.addEventListener('click', clickHandler, { capture: true });
-section.addEventListener('click', clickHandler, { capture: true });
-button.addEventListener('click', clickHandler, { capture: true });
-
-// на сплиття (bubbling)
-button.addEventListener('click', clickHandler);
-section.addEventListener('click', clickHandler);
-body.addEventListener('click', clickHandler);
-
-// Припинення розповсюдження події
-let clickCounter = 0;
-
-body.addEventListener('click', () => {
-  clickCounter++;
-  console.log('clickCounter :>> ', clickCounter);
+div.addEventListener('click', function () {
+  // this.classList.add('div2');
+  // this.classList.remove('div');
+  this.classList.toggle('div2');
 });
 
-button.addEventListener('click', e => {
-  e.stopPropagation(); // припинення розповсюдження події
-});
+div.style.color = 'red';
 
-// body capture
-// section capture
-// btn capture
-
-// btn X
-// section
-// body
+// класи - рядок з іменами класів, розділеними пробілами
+// div.className = div.className + ' div2';
