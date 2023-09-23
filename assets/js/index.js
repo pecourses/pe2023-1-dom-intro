8e2a8d51cab753259df6684b84232fb6ac13377e
Viktoriia <pecourses>
2023-09-23 17:30:23 +0300

    Add bubbling and capturing + stopPropagation

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,41 @@ const body = document.body;
 const section = document.querySelector('section');
 const button = document.querySelector('button');
 
-function btnClickHandler(e) {
-  console.log('this :>> ', this);
-  console.log('e.target :>> ', e.target);
-  console.log('e.currentTarget :>> ', e.currentTarget);
-
-  // звернення до об'єкту, на якому відбулася подія
-  this.style.backgroundColor = 'yellow';
+function clickHandler(e) {
+  console.log('this :>> ', this); // = currentTarget
+  // console.log('e.target :>> ', e.target); // ціль події, найбільш занурений елемент
+  // console.log('e.currentTarget :>> ', e.currentTarget);
+  console.log('--------------------------------------');
 }
 
-button.addEventListener('click', btnClickHandler);
+// Bubbling and capturing / порядок виклику обробників
+
+// на занурення (capturing)
+body.addEventListener('click', clickHandler, { capture: true });
+section.addEventListener('click', clickHandler, { capture: true });
+button.addEventListener('click', clickHandler, { capture: true });
+
+// на сплиття (bubbling)
+button.addEventListener('click', clickHandler);
+section.addEventListener('click', clickHandler);
+body.addEventListener('click', clickHandler);
+
+// Припинення розповсюдження події
+let clickCounter = 0;
+
+body.addEventListener('click', () => {
+  clickCounter++;
+  console.log('clickCounter :>> ', clickCounter);
+});
+
+button.addEventListener('click', e => {
+  e.stopPropagation(); // припинення розповсюдження події
+});
+
+// body capture
+// section capture
+// btn capture
+
+// btn X
+// section
+// body
