3e3da2bd962b630222bab659216a1d4ee19a9d1b
Viktoriia <pecourses>
2023-09-24 16:54:05 +0300

    Refactor user card

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,47 +7,50 @@ const user = {
   photoSrc: 'https://sigc.edu/sigc/ad-sigc/datas/images/userimg.jpg',
 };
 
-// 1 + Створити елемент
-// 3 ~ Задати значення атрибутам
-// 4 ~ Додати класи
-// 5 ~ Додати контент
-// 6 ~ додати обробник на подію
-// 2 + Вбудувати на потрібне місце
-
 const userCardEl = document.createElement('article');
 userCardEl.classList.add('userCard');
 document.body.append(userCardEl);
 
-const userImg = document.createElement('img');
-userImg.src = user.photoSrc;
-userImg.alt = `${user.firstName} ${user.lastName}`;
-userImg.classList.add('userImg');
-userCardEl.append(userImg);
+const userImg = createImage(user);
+const userInfoEl = createUserInfo(user);
+const trashIcon = createTrashIcon();
 
-const userInfoEl = document.createElement('div');
-userInfoEl.classList.add('userInfo');
-userCardEl.append(userInfoEl);
+userCardEl.append(userImg, userInfoEl, trashIcon);
 
-const userNameEl = document.createElement('p');
-userNameEl.classList.add('userName');
-userNameEl.textContent = `${user.firstName} ${user.lastName}`;
-userInfoEl.append(userNameEl);
-
-const userAgeEl = document.createElement('span');
-userAgeEl.classList.add('userAge');
-userAgeEl.textContent = user.age;
-userInfoEl.append(userAgeEl);
-
-const trashIcon = document.createElement('i');
-trashIcon.classList.add('fa-solid');
-trashIcon.classList.add('fa-trash');
-trashIcon.classList.add('trashIcon');
-
-function deleteUser(e) {
-  // userCardEl.remove();
-  // e.target.parentElement.remove();
-  e.target.closest('.userCard').remove();
+function createImage({ photoSrc, firstName, lastName }) {
+  const userImg = document.createElement('img');
+  userImg.src = photoSrc;
+  userImg.alt = `${firstName} ${lastName}`;
+  userImg.classList.add('userImg');
+  return userImg;
 }
-trashIcon.onclick = deleteUser;
 
-userCardEl.append(trashIcon);
+function createUserInfo({ firstName, lastName, age }) {
+  const userInfoEl = document.createElement('div');
+  userInfoEl.classList.add('userInfo');
+
+  const userNameEl = document.createElement('p');
+  userNameEl.classList.add('userName');
+  userNameEl.textContent = `${firstName} ${lastName}`;
+  userInfoEl.append(userNameEl);
+
+  const userAgeEl = document.createElement('span');
+  userAgeEl.classList.add('userAge');
+  userAgeEl.textContent = age;
+  userInfoEl.append(userAgeEl);
+
+  return userInfoEl;
+}
+
+function createTrashIcon() {
+  const trashIcon = document.createElement('i');
+  trashIcon.classList.add('fa-solid');
+  trashIcon.classList.add('fa-trash');
+  trashIcon.classList.add('trashIcon');
+
+  function deleteUser(e) {
+    e.target.closest('.userCard').remove();
+  }
+  trashIcon.onclick = deleteUser;
+  return trashIcon;
+}
