e4350fd2c0401a8bf4093eb5ebb667a1f318581b
Viktoriia <pecourses>
2023-09-24 15:31:16 +0300

    Add user-card base

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,9 @@
 'use strict';
 
-const section = document.createElement('section');
-
-const divRoot = document.querySelector('#root');
-
-// додавання дочірнього ел-та append/prepend
-divRoot.append(section);
-section.textContent = 'Lorem ipsum';
-section.style.backgroundColor = 'grey';
-
-const article = document.createElement('article');
-divRoot.prepend(article);
-article.textContent = 'article is the first child';
-
-// додавання сіблінгів - before/after
-
-const btn = document.createElement('button');
-
-section.after(btn);
-btn.textContent = 'new btn';
-
-// видалення
-const divForRemove = document.querySelector('.removable');
-
-divForRemove.addEventListener('mouseenter', function () {
-  this.remove();
-});
+const user = {
+  firstName: 'Test1',
+  lastName: 'Testovich1',
+  age: 28,
+  photoSrc:
+    'https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg',
+};
