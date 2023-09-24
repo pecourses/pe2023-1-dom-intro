954d9475a2ef3caf3a7d3d8ceb2bd2e9a56dda7d
Viktoriia <pecourses>
2023-09-24 17:10:15 +0300

    Add users list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,57 @@
 'use strict';
 
-const user = {
-  firstName: 'Test1',
-  lastName: 'Testovich1',
-  age: 28,
-  photoSrc: 'https://sigc.edu/sigc/ad-sigc/datas/images/userimg.jpg',
-};
+const users = [
+  {
+    firstName: 'Test1',
+    lastName: 'Testovich1',
+    age: 28,
+    photoSrc: 'https://sigc.edu/sigc/ad-sigc/datas/images/userimg.jpg',
+  },
+  {
+    firstName: 'Test2',
+    lastName: 'Testovich2',
+    age: 31,
+    photoSrc:
+      'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    firstName: 'Test3',
+    lastName: 'Testovich3',
+    age: 70,
+    photoSrc:
+      'https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    firstName: 'Test4',
+    lastName: 'Testovich4',
+    age: 20,
+    photoSrc:
+      'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    firstName: 'Test5',
+    lastName: 'Testovich5',
+    age: 28,
+    photoSrc:
+      'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
 
-const userCardEl = document.createElement('article');
-userCardEl.classList.add('userCard');
-document.body.append(userCardEl);
+const userCards = users.map(u => createUserCard(u));
+document.body.append(...userCards);
+// document.body.append(userCards[0], userCards[1], userCards[2], ...);
 
-const userImg = createImage(user);
-const userInfoEl = createUserInfo(user);
-const trashIcon = createTrashIcon();
+function createUserCard(user) {
+  const userCardEl = document.createElement('article');
+  userCardEl.classList.add('userCard');
 
-userCardEl.append(userImg, userInfoEl, trashIcon);
+  const userImg = createImage(user);
+  const userInfoEl = createUserInfo(user);
+  const trashIcon = createTrashIcon();
+
+  userCardEl.append(userImg, userInfoEl, trashIcon);
+  return userCardEl;
+}
 
 function createImage({ photoSrc, firstName, lastName }) {
   const userImg = document.createElement('img');
